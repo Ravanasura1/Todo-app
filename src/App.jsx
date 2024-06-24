7a8fd5bc5712ac4d96665584d59d0af6506fa148
Shantanu dev upadhyay <shantanudevupadhyay@gmail.com>
2024-06-24 20:01:01 +0530

    complete sign up page ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import SignUp from "./Auth/SignUp";
+
+function App() {
+  return (
+    <>
+      <SignUp />
+    </>
+  );
+}
+
+export default App;
